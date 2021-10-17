b3a1d9ede839f696d2c8f1cc8ad31304a9b79c9d
kakoli306 <kakoli1compete@gmail.com>
2021-10-18 00:05:36 +0600

    design

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ window.Vue = require('vue').default;
 import router from './router'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
-import Vue from 'vue';
+
 Vue.use(ViewUI);
 
 Vue.component('mainapp', require('./components/mainapp.vue').default);
